2ab1fcbfe072c25070688d8856b92d97a68a858f
Ananta Chandra Das <danantach@gmail.com>
2025-05-29 00:37:38 +0530

    Add README.md

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import AppRoutes from './routes/AppRoutes'
 import { UserProvider } from './contexts/userContext'
 import { MeetingProvider } from './contexts/meetingContext'
